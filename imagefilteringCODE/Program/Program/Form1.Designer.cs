299295abb37da8985b10856ef9ee7c840acf1e28
krasnovandr <krasnovandr@mail.ru>
2015-10-05 23:48:47 +0300

    Add matrix filters lab

---
+++ imagefilteringCODE/Program/Program/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.яркостьконтрастностьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.цветовойБалансToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.повыситьРезкостьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.размытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.медианныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.эррозияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.наращиваниеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 112);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(346, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripMenuItem1.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть...";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.яркостьконтрастностьToolStripMenuItem,
+            this.цветовойБалансToolStripMenuItem,
+            this.повыситьРезкостьToolStripMenuItem,
+            this.размытьToolStripMenuItem,
+            this.медианныйToolStripMenuItem,
+            this.эррозияToolStripMenuItem,
+            this.наращиваниеToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(69, 20);
+            this.toolStripMenuItem2.Text = "Фильтры";
+            // 
+            // яркостьконтрастностьToolStripMenuItem
+            // 
+            this.яркостьконтрастностьToolStripMenuItem.Name = "яркостьконтрастностьToolStripMenuItem";
+            this.яркостьконтрастностьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.яркостьконтрастностьToolStripMenuItem.Text = "Яркость/контрастность";
+            this.яркостьконтрастностьToolStripMenuItem.Click += new System.EventHandler(this.яркостьконтрастностьToolStripMenuItem_Click);
+            // 
+            // цветовойБалансToolStripMenuItem
+            // 
+            this.цветовойБалансToolStripMenuItem.Name = "цветовойБалансToolStripMenuItem";
+            this.цветовойБалансToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.цветовойБалансToolStripMenuItem.Text = "Цветовой баланс";
+            this.цветовойБалансToolStripMenuItem.Click += new System.EventHandler(this.цветовойБалансToolStripMenuItem_Click);
+            // 
+            // повыситьРезкостьToolStripMenuItem
+            // 
+            this.повыситьРезкостьToolStripMenuItem.Name = "повыситьРезкостьToolStripMenuItem";
+            this.повыситьРезкостьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.повыситьРезкостьToolStripMenuItem.Text = "Повысить резкость";
+            this.повыситьРезкостьToolStripMenuItem.Click += new System.EventHandler(this.повыситьРезкостьToolStripMenuItem_Click);
+            // 
+            // размытьToolStripMenuItem
+            // 
+            this.размытьToolStripMenuItem.Name = "размытьToolStripMenuItem";
+            this.размытьToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.размытьToolStripMenuItem.Text = "Размыть";
+            this.размытьToolStripMenuItem.Click += new System.EventHandler(this.размытьToolStripMenuItem_Click);
+            // 
+            // медианныйToolStripMenuItem
+            // 
+            this.медианныйToolStripMenuItem.Name = "медианныйToolStripMenuItem";
+            this.медианныйToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.медианныйToolStripMenuItem.Text = "Медианный";
+            this.медианныйToolStripMenuItem.Click += new System.EventHandler(this.медианныйToolStripMenuItem_Click);
+            // 
+            // эррозияToolStripMenuItem
+            // 
+            this.эррозияToolStripMenuItem.Name = "эррозияToolStripMenuItem";
+            this.эррозияToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.эррозияToolStripMenuItem.Text = "Эррозия";
+            this.эррозияToolStripMenuItem.Click += new System.EventHandler(this.эррозияToolStripMenuItem_Click);
+            // 
+            // наращиваниеToolStripMenuItem
+            // 
+            this.наращиваниеToolStripMenuItem.Name = "наращиваниеToolStripMenuItem";
+            this.наращиваниеToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.наращиваниеToolStripMenuItem.Text = "Наращивание";
+            this.наращиваниеToolStripMenuItem.Click += new System.EventHandler(this.наращиваниеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 290);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Фильтрация изображений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem яркостьконтрастностьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem цветовойБалансToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem повыситьРезкостьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem размытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem медианныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem эррозияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem наращиваниеToolStripMenuItem;
+    }
+}
+
---
+++ imagefilteringCODE/Program/Program/MatricaSvertki.cs
@@ -0,0 +1,233 @@
+﻿using System;
+
+namespace Program
+{
+    struct RGB
+    {
+        public float R;
+        public float G;
+        public float B;
+    }
+    public enum Filters
+    {
+        Median,
+        Erosion,
+        BuildUp
+    }
+
+    class Filter
+    {
+        public static UInt32[,] matrix_filtration(int W, int H, UInt32[,] pixel, int N, double[,] matryx)
+        {
+            int gap = (int)(N / 2);
+            int tmpH = H + 2 * gap;
+            int tmpW = W + 2 * gap;
+            UInt32[,] newpixel = new UInt32[H, W];
+
+            var tmppixel = FillTempMatrix(W, H, pixel, tmpH, tmpW, gap);
+            //применение ядра свертки
+            RGB ColorOfPixel = new RGB();
+            RGB ColorOfCell = new RGB();
+            for (int i = gap; i < tmpH - gap; i++)
+                for (int j = gap; j < tmpW - gap; j++)
+                {
+                    ColorOfPixel.R = 0;
+                    ColorOfPixel.G = 0;
+                    ColorOfPixel.B = 0;
+                    for (int k = 0; k < N; k++)
+                        for (int m = 0; m < N; m++)
+                        {
+                            ColorOfCell = calculationOfColor(tmppixel[i - gap + k, j - gap + m], matryx[k, m]);
+                            ColorOfPixel.R += ColorOfCell.R;
+                            ColorOfPixel.G += ColorOfCell.G;
+                            ColorOfPixel.B += ColorOfCell.B;
+                        }
+                    //контролируем переполнение переменных
+                    if (ColorOfPixel.R < 0) ColorOfPixel.R = 0;
+                    if (ColorOfPixel.R > 255) ColorOfPixel.R = 255;
+                    if (ColorOfPixel.G < 0) ColorOfPixel.G = 0;
+                    if (ColorOfPixel.G > 255) ColorOfPixel.G = 255;
+                    if (ColorOfPixel.B < 0) ColorOfPixel.B = 0;
+                    if (ColorOfPixel.B > 255) ColorOfPixel.B = 255;
+
+                    newpixel[i - gap, j - gap] = build(ColorOfPixel);
+                }
+
+            return newpixel;
+        }
+
+        public static uint[,] MedianFiltration(int W, int H, uint[,] pixel, int N, Filters filter)
+        {
+            int gap = N / 2;
+            int tmpH = H + 2 * gap;
+            int tmpW = W + 2 * gap;
+            var newpixel = new UInt32[H, W];
+
+            var tmppixel = FillTempMatrix(W, H, pixel, tmpH, tmpW, gap);
+            //применение ядра свертки
+
+            for (int i = gap; i < tmpH - gap; i++)
+                for (int j = gap; j < tmpW - gap; j++)
+                {
+                    RGB colorOfPixel;
+                    colorOfPixel.R = 0;
+                    colorOfPixel.G = 0;
+                    colorOfPixel.B = 0;
+                    var array = new UInt32[N * N];
+                    int index = 0;
+
+                    for (int k = 0; k < N; k++)
+                    {
+                        for (int m = 0; m < N; m++)
+                        {
+                            array[index++] = tmppixel[i - gap + k, j - gap + m];
+
+                        }
+                    }
+
+                    colorOfPixel = GetResult(array, filter);
+                    //контролируем переполнение переменных
+                    if (colorOfPixel.R < 0) colorOfPixel.R = 0;
+                    if (colorOfPixel.R > 255) colorOfPixel.R = 255;
+                    if (colorOfPixel.G < 0) colorOfPixel.G = 0;
+                    if (colorOfPixel.G > 255) colorOfPixel.G = 255;
+                    if (colorOfPixel.B < 0) colorOfPixel.B = 0;
+                    if (colorOfPixel.B > 255) colorOfPixel.B = 255;
+
+                    newpixel[i - gap, j - gap] = build(colorOfPixel);
+                }
+
+            return newpixel;
+        }
+
+        private static uint[,] FillTempMatrix(int W, int H, uint[,] pixel, int tmpH, int tmpW, int gap)
+        {
+            UInt32[,] tmppixel = new UInt32[tmpH, tmpW];
+
+            //заполнение временного расширенного изображения
+            //углы
+            for (int i = 0; i < gap; i++)
+            {
+                for (int j = 0; j < gap; j++)
+                {
+                    tmppixel[i, j] = pixel[0, 0];
+                    tmppixel[i, tmpW - 1 - j] = pixel[0, W - 1];
+                    tmppixel[tmpH - 1 - i, j] = pixel[H - 1, 0];
+                    tmppixel[tmpH - 1 - i, tmpW - 1 - j] = pixel[H - 1, W - 1];
+                }
+            }
+
+            //крайние левая и правая стороны
+            for (int i = gap; i < tmpH - gap; i++)
+            {
+                for (int j = 0; j < gap; j++)
+                {
+                    tmppixel[i, j] = pixel[i - gap, j];
+                    tmppixel[i, tmpW - 1 - j] = pixel[i - gap, W - 1 - j];
+                }
+            }
+
+            //крайние верхняя и нижняя стороны
+            for (int i = 0; i < gap; i++)
+            {
+                for (int j = gap; j < tmpW - gap; j++)
+                {
+                    tmppixel[i, j] = pixel[i, j - gap];
+                    tmppixel[tmpH - 1 - i, j] = pixel[H - 1 - i, j - gap];
+                }
+            }
+
+            //центр
+            for (int i = 0; i < H; i++)
+            {
+                for (int j = 0; j < W; j++)
+                {
+                    tmppixel[i + gap, j + gap] = pixel[i, j];
+                }
+            }
+
+            return tmppixel;
+        }
+
+        private static RGB GetResult(uint[] array, Filters filter)
+        {
+            var redArray = new UInt32[array.Length];
+            var greenArray = new UInt32[array.Length];
+            var blueArray = new UInt32[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                redArray[i] = (array[i] & 0x00FF0000) >> 16;
+                greenArray[i] = (array[i] & 0x0000FF00) >> 8;
+                blueArray[i] = array[i] & 0x000000FF;
+            }
+            int index = 0;
+            switch (filter)
+            {
+                case Filters.BuildUp:
+                    index = array.Length - 1;
+                    break;
+                case Filters.Erosion:
+                    index = 0;
+                    break;
+                case Filters.Median:
+                    index = array.Length / 2;
+                    break;
+            }
+            Array.Sort(redArray);
+            Array.Sort(greenArray);
+            Array.Sort(blueArray);
+
+            var color = new RGB
+            {
+                R = redArray[index],
+                G = greenArray[index],
+                B = blueArray[index]
+            };
+            return color;
+        }
+
+        //вычисление нового цвета
+        public static RGB calculationOfColor(UInt32 pixel, double coefficient)
+        {
+            RGB Color = new RGB();
+            Color.R = (float)(coefficient * ((pixel & 0x00FF0000) >> 16));
+            Color.G = (float)(coefficient * ((pixel & 0x0000FF00) >> 8));
+            Color.B = (float)(coefficient * (pixel & 0x000000FF));
+            return Color;
+        }
+
+        //сборка каналов
+        public static UInt32 build(RGB ColorOfPixel)
+        {
+            UInt32 Color;
+            Color = 0xFF000000 | ((UInt32)ColorOfPixel.R << 16) | ((UInt32)ColorOfPixel.G << 8) | ((UInt32)ColorOfPixel.B);
+            return Color;
+        }
+
+
+
+
+
+
+
+
+        //повышение резкости
+        public const int N1 = 3;
+        public static double[,] sharpness = new double[N1, N1] {{-1, -1, -1},
+                                                               {-1,  9, -1},
+                                                               {-1, -1, -1}};
+
+        //размытие
+        public const int N2 = 5;
+        public static double[,] blur = new double[N2, N2] {{0.000789, 0.006581, 0.013347,0.006581,0.000789},
+                                                               {0.006581, 0.054901, 0.111345,0.054901,0.006581},
+                                                               {0.013347, 0.111345, 0.225821,0.111345,0.013347},
+                                                               {0.006581, 0.054901, 0.111345,0.054901,0.006581},
+                                                               {0.000789, 0.006581, 0.013347,0.006581,0.000789}};
+
+
+
+
+    }
+}
